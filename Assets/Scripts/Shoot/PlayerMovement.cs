31e94c4b442b6da722ce059a6731d204529b1190
sekarlintang <sinekarlintang@gmail.com>
2022-02-06 09:13:25 +0700

    Player Animation
    HELP KALO PLAYERNYA JALAN, TORCHNYA SUKA KETINGGALAN

--- Assets/Scripts/Shoot/PlayerMovement.cs
+++ Assets/Scripts/Shoot/PlayerMovement.cs
@@ -5,19 +5,38 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public Camera cam;
     public Rigidbody2D rb;
+    public Rigidbody2D rbTorch;
+    public Animator animator;
 
     public Vector2 pos;
+    Vector2 rot;
 
     // Update is called once per frame
     public void Update()
     {
         pos.x = Input.GetAxisRaw("Horizontal");
+        if (Input.GetAxisRaw("Horizontal")>0)
+        {
+            animator.SetBool("Kanan", true);
+        }
         pos.y = Input.GetAxisRaw("Vertical");
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            animator.SetBool("Kiri", true);
+        }
+
+        rot = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
-    private void FixedUpdate()
+    public void FixedUpdate()
     {
         rb.MovePosition(rb.position + pos * moveSpeed * Time.fixedDeltaTime);
+        rbTorch.MovePosition(rbTorch.position + pos * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = rot - rbTorch.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rbTorch.rotation = angle;
     }
 }
--- Assets/TorchMovement.cs
+++ Assets/TorchMovement.cs
@@ -4,25 +4,11 @@ using UnityEngine;
 
 public class TorchMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public Camera cam;
-    public Rigidbody2D rb;
     public GameObject Player;
-    Vector2 rot;
-    Vector2 pos;
-
-    void Update()
+    
+    void Follow()
     {
-        pos = Player.GetComponent<PlayerMovement>().pos;
-        rot = cam.ScreenToWorldPoint(Input.mousePosition);
-    }
-    private void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + pos * moveSpeed * Time.fixedDeltaTime);
-
-        Vector2 lookDir = rot - rb.position;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = angle;
+        transform.position = Player.transform.position;
     }
 }
 
